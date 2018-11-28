c2627894391f4fd6f9483db5d86c4c6f0f434444
Ryna <pinktulips@Hotmail.com>
2018-11-28 09:38:00 -0800

    make constructor

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # lab-02
 Lab 02: jQuery Selectors and Events (Zubatch, Truong, Teague)
+
+Number and name of feature:
+EStimate of time needed to complete:
+Start time:
+Finish time:
+Actual time needed to complete:
+
+
---
+++ js/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function Animal(animal) {
+  this.image_url = animal.image_url;
+  this.title = animal.title;
+  this.description = animal.description;
+  this.keyword = animal.keyword;
+  this.horns = animal.horns;
+}
+
+Animal.allAnimals = [];
+
+
+
